a310ca21f5885a120b45d703165ed9dc2de74f06
blagojevic-nj <blagojevic.njegos1999@gmail.com>
2023-07-31 16:50:30 +0200

    fix: remove cmd runner

--- src/main/java/buky/example/notificationservice/NotificationServiceApplication.java
+++ src/main/java/buky/example/notificationservice/NotificationServiceApplication.java
@@ -1,31 +1,13 @@
 package buky.example.notificationservice;
 
-import buky.example.notificationservice.model.Notification;
-import buky.example.notificationservice.model.enums.NotificationType;
-import buky.example.notificationservice.service.NotificationService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.LocalDateTime;
-
 @SpringBootApplication
-@RequiredArgsConstructor
-public class NotificationServiceApplication implements CommandLineRunner {
-
-	private final NotificationService notificationService;
+public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationServiceApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) {
-		Notification notification = Notification.builder().notificationType(NotificationType.HOST_RATING)
-				.createdAt(LocalDateTime.now()).processed(false).receiverId(1L).subjectId(1L).build();
-
-		notificationService.saveNotification(notification);
-
-	}
 }
