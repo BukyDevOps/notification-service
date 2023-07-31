fc1f562a78ababf4800abef9da98344336b8daa9
blagojevic-nj <blagojevic.njegos1999@gmail.com>
2023-07-31 16:22:54 +0200

    feat: database setup implemented

---
+++ src/main/java/buky/example/notificationservice/repository/NotificationRepository.java
@@ -0,0 +1,9 @@
+package buky.example.notificationservice.repository;
+
+import buky.example.notificationservice.model.Notification;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends MongoRepository<Notification, String> {
+}
