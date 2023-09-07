dad8bf36ec7a3c04b4317721067b6bbef605dcfa
blagojevic-nj <66080426+blagojevic-nj@users.noreply.github.com>
2023-09-07 18:25:14 +0200

    fix: socket fixed (#10)
    * fix: socket fixed
    
    * conflicts resolved

--- src/main/java/buky/example/notificationservice/config/SocketConfig.java
+++ src/main/java/buky/example/notificationservice/config/SocketConfig.java
@@ -11,7 +11,7 @@ public class SocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/socket")
-                .setAllowedOrigins("*")
+                .setAllowedOrigins("http://localhost:3001")
                 .withSockJS();
     }
 
